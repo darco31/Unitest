1cd4a02309b503606b96969c9c6eafe56a0d290a
Stephen <sdarcy29@hotmail.com>
2022-02-05 19:47:20 +0000

    First test file creation

---
+++ test_evens.py
@@ -0,0 +1,13 @@
+"""
+Imports unittest to test file
+"""
+import unittest
+from evens import even_number_of_evens
+
+
+class TestEvens(unittest.TestCase):
+    
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
